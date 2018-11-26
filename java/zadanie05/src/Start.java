17bf291f77f7a7c3a8528aed5ed1c060359e367d
m1nified <m1nified.dev@gmail.com>
2018-11-26 03:58:05 +0100

    Passing test.

--- java/zadanie05/src/Start.java
+++ java/zadanie05/src/Start.java
@@ -1,3 +1,9 @@
+import java.util.Comparator;
+import java.util.Iterator;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentSkipListSet;
+import java.util.concurrent.atomic.AtomicInteger;
+
 import org.omg.CORBA.IntHolder;
 import org.omg.CosNaming.NameComponent;
 import org.omg.CosNaming.NamingContext;
@@ -7,21 +13,115 @@ import org.omg.PortableServer.POA;
 import com.sun.corba.se.org.omg.CORBA.ORB;
 
 class OptimizationImpl extends optimizationPOA implements optimizationOperations {
+	
+	private final static boolean DEBUG = false;
+
+	class ServerItem {
+		private short ip;
+		private int id;
+		private int timeout;
+		private long lastHello;
+
+		public ServerItem(int id, short ip, int timeout) {
+			this.id = id;
+			this.ip = ip;
+			this.timeout = timeout;
+		}
+
+		public short getIp() {
+			return ip;
+		}
+
+		public int getId() {
+			return id;
+		}
+
+		public int getTimeout() {
+			return timeout;
+		}
+
+		public void setTimeout(int timeout) {
+			this.timeout = timeout;
+		}
+
+		public void hello() {
+			lastHello = System.currentTimeMillis();
+		}
+
+		public boolean isActive() {
+			return System.currentTimeMillis() - lastHello < timeout;
+		}
+	}
+
+	class ServerItemIpComparator implements Comparator<ServerItem> {
+
+		@Override
+		public int compare(ServerItem o1, ServerItem o2) {
+			return o1.getIp() - o2.getIp();
+		}
+
+	}
+
+	static AtomicInteger idCount = new AtomicInteger(0);
+
+	private ConcurrentHashMap<Integer, ServerItem> servers = new ConcurrentHashMap<Integer, ServerItem>();
+	private ConcurrentHashMap<Short, ServerItem> ipServerMap = new ConcurrentHashMap<Short, ServerItem>();
+	private ConcurrentSkipListSet<ServerItem> serversList = new ConcurrentSkipListSet<ServerItem>(
+			new ServerItemIpComparator());
 
 	@Override
 	public void register(short ip, int timeout, IntHolder id) {
-		System.out.println("OptimizationImpl/register " + ip + " " + timeout + " " + id);
+		if(DEBUG) System.out.println("OptimizationImpl/register " + ip + " " + timeout + " " + id);
+		ServerItem serverItem = ipServerMap.get(ip);
+		if (serverItem != null) {
+			serverItem.setTimeout(timeout);
+			id.value = serverItem.getId();
+		} else {
+			id.value = idCount.getAndIncrement();
+			serverItem = new ServerItem(id.value, ip, timeout);
+			serverItem.hello();
+			ipServerMap.put(ip, serverItem);
+			servers.put(id.value, serverItem);
+			serversList.add(serverItem);
+		}
+
 	}
 
 	@Override
 	public void hello(int id) {
-		System.out.println("OptimizationImpl/hello " + id);
+		if(DEBUG) System.out.println("OptimizationImpl/hello " + id);
+		ServerItem serverItem = servers.get(id);
+		if (serverItem != null) {
+			serverItem.hello();
+		}
 	}
 
 	@Override
 	public void best_range(rangeHolder r) {
-		System.out.println("OptimizationImpl/best_range " + r);
-		r.value = new range((short)1,(short)2);
+		if(DEBUG) System.out.println("OptimizationImpl/best_range " + r);
+
+		range bestRange = null, tmpRange = null;
+
+		Iterator<ServerItem> it = serversList.iterator();
+		while (it.hasNext()) {
+			ServerItem sItem = it.next();
+			if (tmpRange == null && sItem.isActive()) {
+				tmpRange = new range(sItem.getIp(), sItem.getIp());
+			} else if (tmpRange != null && sItem.isActive()) {
+				if (sItem.getIp() - 1 == tmpRange.to) {
+					tmpRange.to += 1;
+				} else {
+					tmpRange = new range(sItem.getIp(), sItem.getIp());
+				}
+			} else {
+				tmpRange = null;
+			}
+			if (bestRange == null || tmpRange != null && tmpRange.to - tmpRange.from > bestRange.to - bestRange.from) {
+				bestRange = tmpRange;
+			}
+		}
+
+		r.value = bestRange;
 	}
 }
 
@@ -40,7 +140,7 @@ class Start {
 
 			org.omg.CORBA.Object namingContextObj = orb.resolve_initial_references("NameService");
 			NamingContext nCont = NamingContextHelper.narrow(namingContextObj);
-			NameComponent[] path = { new NameComponent("Optymalizacja", "OptimizationImpl") };
+			NameComponent[] path = { new NameComponent("Optymalizacja", "Object") };
 
 			nCont.rebind(path, ref);
 			orb.run();
